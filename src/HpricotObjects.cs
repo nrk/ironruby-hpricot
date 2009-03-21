db0b0d60790b91557a28e8a8bf2ae21ffd3bb4e9
Daniele Alessandri <suppakilla@gmail.com>
2009-03-21 16:38:56 +0100

    Added HpricotState class

--- src/HpricotObjects.cs
+++ src/HpricotObjects.cs
@@ -4,6 +4,16 @@ namespace IronRuby.Libraries.Hpricot {
 
     // TODO: I am starting off with this, it will be refactored later.
 
+    public class HpricotState {
+        public Object doc;
+        public Object focus;
+        public Object last;
+        public Object EC;
+        public Char xml;
+        public Char strict;
+        public Char fixup;
+    }
+
     public class HpricotBasic {
         public Object tag;
         public Object parent;
