83e0686e6c108200a0f73254966864afc880af35
Daniele Alessandri <suppakilla@gmail.com>
2009-03-21 16:20:16 +0100

    Added a first and pretty bare implementation for HpricotBasic, HpricotAttr and HpricotElement classes.

---
+++ src/HpricotObjects.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IronRuby.Libraries.Hpricot {
+
+    // TODO: I am starting off with this, it will be refactored later.
+
+    public class HpricotBasic {
+        public Object tag;
+        public Object parent;
+    }
+
+    public class HpricotAttr : HpricotBasic {
+        public Object attr;
+    }
+
+    public class HpricotElement : HpricotBasic {
+        public Object children;
+        public Object etag;
+        public Object raw;
+        public Object EC;
+        public Int32 name;
+    }
+}
