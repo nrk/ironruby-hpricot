ca22cd5c0898cd195c8501dadfc136f3216ece2c
Daniele Alessandri <suppakilla@gmail.com>
2009-03-22 11:14:17 +0100

    Refactored ScannerState

--- src/ScannerState.cs
+++ src/ScannerState.cs
@@ -1,16 +1,69 @@
 ﻿using System;
+using IronRuby.Builtins;
 
 namespace IronRuby.Libraries.Hpricot {
-
-    // TODO: I am starting off with this, it will be refactored later.
-
     public class ScannerState {
-        public Object doc;
-        public Object focus;
-        public Object last;
-        public Object EC;
-        public bool xml;
-        public bool strict;
-        public bool fixup;
+        #region fields
+
+        private Object _doc;
+        private Object _focus;
+        private Object _last;
+        private Object _EC;
+        private bool _xml;
+        private bool _strict;
+        private bool _fixup;
+
+        #endregion
+
+        #region constructors
+
+        public ScannerState() {
+            _xml = false;
+            _strict = false;
+            _fixup = false;
+        }
+
+        #endregion
+
+        #region properties 
+
+        public Object Doc {
+            get { return _doc; }
+            set { _doc = value; }
+        }
+
+        public Object Focus {
+            get { return _focus; }
+            set { _focus = value; }
+        }
+
+        public Object Last {
+            get { return _last; }
+            set { _last = value; }
+        }
+
+        public Object EC {
+            get { return _EC; }
+            set { _EC = value; }
+        }
+
+        public bool Xml {
+            get { return _xml; }
+            set { _xml = value; }
+        }
+
+        public bool Strict {
+            get { return _strict; }
+
+            //TODO: setting Strict to true should set Fixup to true automatically.
+            set { _strict = value; }
+        }
+        
+        public bool Fixup {
+            get { return _fixup; }
+            set { _fixup = value; }
+        }
+
+        #endregion
     }
 }
