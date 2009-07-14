114fb7b7a8985bb704fa073a6384b53593ed1660
Daniele Alessandri <suppakilla@gmail.com>
2009-07-14 20:55:11 +0200

    Just updated the Initializers.Generated.cs. Bumped the compatibility version up to IronRuby 0.6.

--- src/Initializers.Generated.cs
+++ src/Initializers.Generated.cs
@@ -7,38 +7,38 @@ namespace IronRuby.Libraries.Hpricot {
             IronRuby.Builtins.RubyClass classRef1 = GetClass(typeof(System.SystemException));
             
             
-            IronRuby.Builtins.RubyModule def1 = DefineGlobalModule("Hpricot", typeof(IronRuby.Libraries.Hpricot.Hpricot), true, null, LoadHpricot_Class, LoadHpricot_Constants, IronRuby.Builtins.RubyModule.EmptyArray);
-            IronRuby.Builtins.RubyClass def2 = DefineClass("Hpricot::BaseEle", typeof(IronRuby.Libraries.Hpricot.Hpricot.BaseElement), true, classRef0, LoadHpricot__BaseEle_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray);
-            IronRuby.Builtins.RubyClass def6 = DefineClass("Hpricot::Doc", typeof(IronRuby.Libraries.Hpricot.Hpricot.Document), true, classRef0, LoadHpricot__Doc_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
+            IronRuby.Builtins.RubyModule def1 = DefineGlobalModule("Hpricot", typeof(IronRuby.Libraries.Hpricot.Hpricot), 0x00000103, null, LoadHpricot_Class, LoadHpricot_Constants, IronRuby.Builtins.RubyModule.EmptyArray);
+            IronRuby.Builtins.RubyClass def2 = DefineClass("Hpricot::BaseEle", typeof(IronRuby.Libraries.Hpricot.Hpricot.BaseElement), 0x00000103, classRef0, LoadHpricot__BaseEle_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray);
+            IronRuby.Builtins.RubyClass def6 = DefineClass("Hpricot::Doc", typeof(IronRuby.Libraries.Hpricot.Hpricot.Document), 0x00000103, classRef0, LoadHpricot__Doc_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
                 new System.Func<IronRuby.Builtins.RubyClass, IronRuby.Libraries.Hpricot.Hpricot.Document>(IronRuby.Libraries.Hpricot.Hpricot.Document.Allocator)
             );
-            IronRuby.Builtins.RubyClass def10 = DefineClass("Hpricot::ParseError", typeof(IronRuby.Libraries.Hpricot.Hpricot.ParserException), true, classRef1, null, null, null, IronRuby.Builtins.RubyModule.EmptyArray);
+            IronRuby.Builtins.RubyClass def10 = DefineClass("Hpricot::ParseError", typeof(IronRuby.Libraries.Hpricot.Hpricot.ParserException), 0x00000103, classRef1, null, null, null, IronRuby.Builtins.RubyModule.EmptyArray);
             ExtendModule(typeof(IronRuby.Builtins.MutableString), LoadIronRuby__Builtins__MutableString_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray);
-            IronRuby.Builtins.RubyClass def4 = DefineClass("Hpricot::CData", typeof(IronRuby.Libraries.Hpricot.Hpricot.CData), true, def2, LoadHpricot__CData_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
+            IronRuby.Builtins.RubyClass def4 = DefineClass("Hpricot::CData", typeof(IronRuby.Libraries.Hpricot.Hpricot.CData), 0x00000103, def2, LoadHpricot__CData_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
                 new System.Func<IronRuby.Builtins.RubyClass, IronRuby.Libraries.Hpricot.Hpricot.CData>(IronRuby.Libraries.Hpricot.Hpricot.CData.Allocator)
             );
-            IronRuby.Builtins.RubyClass def5 = DefineClass("Hpricot::Comment", typeof(IronRuby.Libraries.Hpricot.Hpricot.Comment), true, def2, LoadHpricot__Comment_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
+            IronRuby.Builtins.RubyClass def5 = DefineClass("Hpricot::Comment", typeof(IronRuby.Libraries.Hpricot.Hpricot.Comment), 0x00000103, def2, LoadHpricot__Comment_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
                 new System.Func<IronRuby.Builtins.RubyClass, IronRuby.Libraries.Hpricot.Hpricot.Comment>(IronRuby.Libraries.Hpricot.Hpricot.Comment.Allocator)
             );
-            IronRuby.Builtins.RubyClass def7 = DefineClass("Hpricot::DocType", typeof(IronRuby.Libraries.Hpricot.Hpricot.DocumentType), true, def2, LoadHpricot__DocType_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
+            IronRuby.Builtins.RubyClass def7 = DefineClass("Hpricot::DocType", typeof(IronRuby.Libraries.Hpricot.Hpricot.DocumentType), 0x00000103, def2, LoadHpricot__DocType_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
                 new System.Func<IronRuby.Builtins.RubyClass, IronRuby.Libraries.Hpricot.Hpricot.DocumentType>(IronRuby.Libraries.Hpricot.Hpricot.DocumentType.Allocator)
             );
-            IronRuby.Builtins.RubyClass def8 = DefineClass("Hpricot::Elem", typeof(IronRuby.Libraries.Hpricot.Hpricot.Element), true, def2, LoadHpricot__Elem_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
+            IronRuby.Builtins.RubyClass def8 = DefineClass("Hpricot::Elem", typeof(IronRuby.Libraries.Hpricot.Hpricot.Element), 0x00000103, def2, LoadHpricot__Elem_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
                 new System.Func<IronRuby.Builtins.RubyClass, IronRuby.Libraries.Hpricot.Hpricot.Element>(IronRuby.Libraries.Hpricot.Hpricot.Element.Allocator)
             );
-            IronRuby.Builtins.RubyClass def9 = DefineClass("Hpricot::ETag", typeof(IronRuby.Libraries.Hpricot.Hpricot.ETag), true, def2, LoadHpricot__ETag_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
+            IronRuby.Builtins.RubyClass def9 = DefineClass("Hpricot::ETag", typeof(IronRuby.Libraries.Hpricot.Hpricot.ETag), 0x00000103, def2, LoadHpricot__ETag_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
                 new System.Func<IronRuby.Builtins.RubyClass, IronRuby.Libraries.Hpricot.Hpricot.ETag>(IronRuby.Libraries.Hpricot.Hpricot.ETag.Allocator)
             );
-            IronRuby.Builtins.RubyClass def11 = DefineClass("Hpricot::ProcIns", typeof(IronRuby.Libraries.Hpricot.Hpricot.ProcedureInstruction), true, def2, LoadHpricot__ProcIns_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
+            IronRuby.Builtins.RubyClass def11 = DefineClass("Hpricot::ProcIns", typeof(IronRuby.Libraries.Hpricot.Hpricot.ProcedureInstruction), 0x00000103, def2, LoadHpricot__ProcIns_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
                 new System.Func<IronRuby.Builtins.RubyClass, IronRuby.Libraries.Hpricot.Hpricot.ProcedureInstruction>(IronRuby.Libraries.Hpricot.Hpricot.ProcedureInstruction.Allocator)
             );
-            IronRuby.Builtins.RubyClass def12 = DefineClass("Hpricot::Text", typeof(IronRuby.Libraries.Hpricot.Hpricot.Text), true, def2, LoadHpricot__Text_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
+            IronRuby.Builtins.RubyClass def12 = DefineClass("Hpricot::Text", typeof(IronRuby.Libraries.Hpricot.Hpricot.Text), 0x00000103, def2, LoadHpricot__Text_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
                 new System.Func<IronRuby.Builtins.RubyClass, IronRuby.Libraries.Hpricot.Hpricot.Text>(IronRuby.Libraries.Hpricot.Hpricot.Text.Allocator)
             );
-            IronRuby.Builtins.RubyClass def13 = DefineClass("Hpricot::XMLDecl", typeof(IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration), true, def2, LoadHpricot__XMLDecl_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
+            IronRuby.Builtins.RubyClass def13 = DefineClass("Hpricot::XMLDecl", typeof(IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration), 0x00000103, def2, LoadHpricot__XMLDecl_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
                 new System.Func<IronRuby.Builtins.RubyClass, IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration>(IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration.Allocator)
             );
-            IronRuby.Builtins.RubyClass def3 = DefineClass("Hpricot::BogusETag", typeof(IronRuby.Libraries.Hpricot.Hpricot.BogusETag), true, def9, null, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
+            IronRuby.Builtins.RubyClass def3 = DefineClass("Hpricot::BogusETag", typeof(IronRuby.Libraries.Hpricot.Hpricot.BogusETag), 0x00000103, def9, null, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
                 new System.Func<IronRuby.Builtins.RubyClass, IronRuby.Libraries.Hpricot.Hpricot.BogusETag>(IronRuby.Libraries.Hpricot.Hpricot.BogusETag.Allocator)
             );
             def1.SetConstant("BaseEle", def2);
