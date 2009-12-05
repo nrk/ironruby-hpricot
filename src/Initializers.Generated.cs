e274b4c3afdedea54c3b7bffc4c25af976ce9c44
Daniele Alessandri <suppakilla@gmail.com>
2009-12-05 10:03:12 +0100

    Bumped the compatibility version to IronRuby 0.9.3.

--- src/Initializers.Generated.cs
+++ src/Initializers.Generated.cs
@@ -44,240 +44,240 @@ namespace IronRuby.Libraries.Hpricot {
             IronRuby.Builtins.RubyClass def3 = DefineClass("Hpricot::BogusETag", typeof(IronRuby.Libraries.Hpricot.Hpricot.BogusETag), 0x00000100, def9, null, null, null, IronRuby.Builtins.RubyModule.EmptyArray, 
                 new Func<IronRuby.Builtins.RubyClass, IronRuby.Libraries.Hpricot.Hpricot.BogusETag>(IronRuby.Libraries.Hpricot.Hpricot.BogusETag.Allocator)
             );
-            def1.SetConstant("BaseEle", def2);
-            def1.SetConstant("Doc", def6);
-            def1.SetConstant("ParseError", def10);
-            def1.SetConstant("CData", def4);
-            def1.SetConstant("Comment", def5);
-            def1.SetConstant("DocType", def7);
-            def1.SetConstant("Elem", def8);
-            def1.SetConstant("ETag", def9);
-            def1.SetConstant("ProcIns", def11);
-            def1.SetConstant("Text", def12);
-            def1.SetConstant("XMLDecl", def13);
-            def1.SetConstant("BogusETag", def3);
+            SetConstant(def1, "BaseEle", def2);
+            SetConstant(def1, "Doc", def6);
+            SetConstant(def1, "ParseError", def10);
+            SetConstant(def1, "CData", def4);
+            SetConstant(def1, "Comment", def5);
+            SetConstant(def1, "DocType", def7);
+            SetConstant(def1, "Elem", def8);
+            SetConstant(def1, "ETag", def9);
+            SetConstant(def1, "ProcIns", def11);
+            SetConstant(def1, "Text", def12);
+            SetConstant(def1, "XMLDecl", def13);
+            SetConstant(def1, "BogusETag", def3);
         }
         
         private static void LoadHpricot_Constants(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.SetConstant("ProcInsParse", IronRuby.Libraries.Hpricot.Hpricot.ProcInsParse);
+            SetConstant(module, "ProcInsParse", IronRuby.Libraries.Hpricot.Hpricot.ProcInsParse);
             
         }
         
         private static void LoadHpricot_Class(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("buffer_size", 0x21, 
+            DefineLibraryMethod(module, "buffer_size", 0x21, 
                 new Func<IronRuby.Builtins.RubyModule, System.Nullable<System.Int32>>(IronRuby.Libraries.Hpricot.Hpricot.GetBufferSize)
             );
             
-            module.DefineLibraryMethod("buffer_size=", 0x21, 
+            DefineLibraryMethod(module, "buffer_size=", 0x21, 
                 new Action<IronRuby.Builtins.RubyModule, System.Int32>(IronRuby.Libraries.Hpricot.Hpricot.SetBufferSize)
             );
             
-            module.DefineLibraryMethod("css", 0x21, 
+            DefineLibraryMethod(module, "css", 0x21, 
                 new Func<IronRuby.Runtime.RubyContext, IronRuby.Runtime.BlockParam, IronRuby.Builtins.RubyModule, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Css)
             );
             
-            module.DefineLibraryMethod("scan", 0x21, 
+            DefineLibraryMethod(module, "scan", 0x21, 
                 new Func<IronRuby.Runtime.ConversionStorage<IronRuby.Builtins.MutableString>, IronRuby.Runtime.RespondToStorage, IronRuby.Runtime.BinaryOpStorage, IronRuby.Runtime.BlockParam, IronRuby.Builtins.RubyModule, System.Object, IronRuby.Builtins.Hash, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Scan)
             );
             
         }
         
         private static void LoadHpricot__BaseEle_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("parent", 0x11, 
+            DefineLibraryMethod(module, "parent", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.BaseElement, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.BaseElement.GetParent)
             );
             
-            module.DefineLibraryMethod("parent=", 0x11, 
+            DefineLibraryMethod(module, "parent=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.BaseElement, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.BaseElement.SetParent)
             );
             
-            module.DefineLibraryMethod("raw_string", 0x11, 
+            DefineLibraryMethod(module, "raw_string", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.BaseElement, IronRuby.Builtins.MutableString>(IronRuby.Libraries.Hpricot.Hpricot.BaseElement.GetRawString)
             );
             
         }
         
         private static void LoadHpricot__CData_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("content", 0x11, 
+            DefineLibraryMethod(module, "content", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.CData, IronRuby.Builtins.MutableString>(IronRuby.Libraries.Hpricot.Hpricot.CData.GetContent)
             );
             
-            module.DefineLibraryMethod("content=", 0x11, 
+            DefineLibraryMethod(module, "content=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.CData, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.CData.SetContent)
             );
             
         }
         
         private static void LoadHpricot__Comment_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("content", 0x11, 
+            DefineLibraryMethod(module, "content", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.Comment, IronRuby.Builtins.MutableString>(IronRuby.Libraries.Hpricot.Hpricot.Comment.GetContent)
             );
             
-            module.DefineLibraryMethod("content=", 0x11, 
+            DefineLibraryMethod(module, "content=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.Comment, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Comment.SetContent)
             );
             
         }
         
         private static void LoadHpricot__Doc_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("children", 0x11, 
+            DefineLibraryMethod(module, "children", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.Document, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Document.GetChildren)
             );
             
-            module.DefineLibraryMethod("children=", 0x11, 
+            DefineLibraryMethod(module, "children=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.Document, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Document.SetChildren)
             );
             
         }
         
         private static void LoadHpricot__DocType_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("public_id", 0x11, 
+            DefineLibraryMethod(module, "public_id", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.DocumentType, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.DocumentType.GetPublicId)
             );
             
-            module.DefineLibraryMethod("public_id=", 0x11, 
+            DefineLibraryMethod(module, "public_id=", 0x11, 
                 new Action<IronRuby.Runtime.RubyContext, IronRuby.Libraries.Hpricot.Hpricot.DocumentType, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.DocumentType.SetPublicId)
             );
             
-            module.DefineLibraryMethod("system_id", 0x11, 
+            DefineLibraryMethod(module, "system_id", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.DocumentType, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.DocumentType.GetSystemId)
             );
             
-            module.DefineLibraryMethod("system_id=", 0x11, 
+            DefineLibraryMethod(module, "system_id=", 0x11, 
                 new Action<IronRuby.Runtime.RubyContext, IronRuby.Libraries.Hpricot.Hpricot.DocumentType, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.DocumentType.SetSystemId)
             );
             
-            module.DefineLibraryMethod("target", 0x11, 
+            DefineLibraryMethod(module, "target", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.DocumentType, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.DocumentType.GetTarget)
             );
             
-            module.DefineLibraryMethod("target=", 0x11, 
+            DefineLibraryMethod(module, "target=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.DocumentType, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.DocumentType.SetTarget)
             );
             
         }
         
         private static void LoadHpricot__Elem_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("children", 0x11, 
+            DefineLibraryMethod(module, "children", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.Element, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Element.GetChildren)
             );
             
-            module.DefineLibraryMethod("children=", 0x11, 
+            DefineLibraryMethod(module, "children=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.Element, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Element.SetChildren)
             );
             
-            module.DefineLibraryMethod("clear_raw", 0x11, 
+            DefineLibraryMethod(module, "clear_raw", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.Element, System.Boolean>(IronRuby.Libraries.Hpricot.Hpricot.Element.ClearRaw)
             );
             
-            module.DefineLibraryMethod("etag", 0x11, 
+            DefineLibraryMethod(module, "etag", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.Element, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Element.GetEtag)
             );
             
-            module.DefineLibraryMethod("etag=", 0x11, 
+            DefineLibraryMethod(module, "etag=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.Element, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Element.SetEtag)
             );
             
-            module.DefineLibraryMethod("name", 0x11, 
+            DefineLibraryMethod(module, "name", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.Element, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Element.GetName)
             );
             
-            module.DefineLibraryMethod("name=", 0x11, 
+            DefineLibraryMethod(module, "name=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.Element, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Element.SetName)
             );
             
-            module.DefineLibraryMethod("raw_attributes", 0x11, 
+            DefineLibraryMethod(module, "raw_attributes", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.Element, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Element.GetRawAttributes)
             );
             
-            module.DefineLibraryMethod("raw_attributes=", 0x11, 
+            DefineLibraryMethod(module, "raw_attributes=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.Element, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Element.SetRawAttributes)
             );
             
-            module.DefineLibraryMethod("raw_string", 0x11, 
+            DefineLibraryMethod(module, "raw_string", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.Element, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Element.GetRawString)
             );
             
         }
         
         private static void LoadHpricot__ETag_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("clear_raw", 0x11, 
+            DefineLibraryMethod(module, "clear_raw", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.ETag, System.Boolean>(IronRuby.Libraries.Hpricot.Hpricot.ETag.ClearRaw)
             );
             
-            module.DefineLibraryMethod("name", 0x11, 
+            DefineLibraryMethod(module, "name", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.ETag, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.ETag.GetName)
             );
             
-            module.DefineLibraryMethod("name=", 0x11, 
+            DefineLibraryMethod(module, "name=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.ETag, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.ETag.SetName)
             );
             
-            module.DefineLibraryMethod("raw_string", 0x11, 
+            DefineLibraryMethod(module, "raw_string", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.ETag, IronRuby.Builtins.MutableString>(IronRuby.Libraries.Hpricot.Hpricot.ETag.GetRawString)
             );
             
         }
         
         private static void LoadHpricot__ProcIns_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("content", 0x11, 
+            DefineLibraryMethod(module, "content", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.ProcedureInstruction, IronRuby.Builtins.MutableString>(IronRuby.Libraries.Hpricot.Hpricot.ProcedureInstruction.GetContent)
             );
             
-            module.DefineLibraryMethod("content=", 0x11, 
+            DefineLibraryMethod(module, "content=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.ProcedureInstruction, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.ProcedureInstruction.SetContent)
             );
             
-            module.DefineLibraryMethod("target", 0x11, 
+            DefineLibraryMethod(module, "target", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.ProcedureInstruction, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.ProcedureInstruction.GetTarget)
             );
             
-            module.DefineLibraryMethod("target=", 0x11, 
+            DefineLibraryMethod(module, "target=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.ProcedureInstruction, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.ProcedureInstruction.SetTarget)
             );
             
         }
         
         private static void LoadHpricot__Text_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("content", 0x11, 
+            DefineLibraryMethod(module, "content", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.Text, IronRuby.Builtins.MutableString>(IronRuby.Libraries.Hpricot.Hpricot.Text.GetContent)
             );
             
-            module.DefineLibraryMethod("content=", 0x11, 
+            DefineLibraryMethod(module, "content=", 0x11, 
                 new Action<IronRuby.Libraries.Hpricot.Hpricot.Text, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Text.SetContent)
             );
             
         }
         
         private static void LoadHpricot__XMLDecl_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("encoding", 0x11, 
+            DefineLibraryMethod(module, "encoding", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration.GetEncoding)
             );
             
-            module.DefineLibraryMethod("encoding=", 0x11, 
+            DefineLibraryMethod(module, "encoding=", 0x11, 
                 new Action<IronRuby.Runtime.RubyContext, IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration.SetEncoding)
             );
             
-            module.DefineLibraryMethod("standalone", 0x11, 
+            DefineLibraryMethod(module, "standalone", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration.GetStandalone)
             );
             
-            module.DefineLibraryMethod("standalone=", 0x11, 
+            DefineLibraryMethod(module, "standalone=", 0x11, 
                 new Action<IronRuby.Runtime.RubyContext, IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration.SetStandalone)
             );
             
-            module.DefineLibraryMethod("version", 0x11, 
+            DefineLibraryMethod(module, "version", 0x11, 
                 new Func<IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration.GetVersion)
             );
             
-            module.DefineLibraryMethod("version=", 0x11, 
+            DefineLibraryMethod(module, "version=", 0x11, 
                 new Action<IronRuby.Runtime.RubyContext, IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.XmlDeclaration.SetVersion)
             );
             
         }
         
         private static void LoadIronRuby__Builtins__MutableString_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("fast_xs", 0x11, 
+            DefineLibraryMethod(module, "fast_xs", 0x11, 
                 new Func<IronRuby.Runtime.RubyContext, IronRuby.Builtins.MutableString, IronRuby.Builtins.MutableString>(IronRuby.Libraries.Hpricot.MutableStringOps.FastXs)
             );
             
