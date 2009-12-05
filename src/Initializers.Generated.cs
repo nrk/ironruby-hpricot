c0f2604e4992d7cc8c690ca1f76bc6d1d35923ac
Daniele Alessandri <suppakilla@gmail.com>
2009-12-05 10:08:07 +0100

    Bumped the compatibility version to IronRuby 0.9.3.

--- src/Initializers.Generated.cs
+++ src/Initializers.Generated.cs
@@ -12,26 +12,26 @@ namespace IronRuby.Libraries.Hpricot {
             IronRuby.Builtins.RubyModule def1 = DefineGlobalModule("Hpricot", typeof(IronRuby.Libraries.Hpricot.Hpricot), 0x00000100, null, LoadHpricot_Class, null, IronRuby.Builtins.RubyModule.EmptyArray);
             IronRuby.Builtins.RubyClass def2 = DefineClass("Hpricot::ParseError", typeof(IronRuby.Libraries.Hpricot.Hpricot.ParserException), 0x00000100, classRef0, null, null, null, IronRuby.Builtins.RubyModule.EmptyArray);
             ExtendModule(typeof(IronRuby.Builtins.MutableString), 0x00000000, LoadIronRuby__Builtins__MutableString_Instance, null, null, IronRuby.Builtins.RubyModule.EmptyArray);
-            def1.SetConstant("ParseError", def2);
+            SetConstant(def1, "ParseError", def2);
         }
         
         private static void LoadHpricot_Class(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("buffer_size", 0x21, 
+            DefineLibraryMethod(module, "buffer_size", 0x21, 
                 new Func<IronRuby.Builtins.RubyModule, System.Nullable<System.Int32>>(IronRuby.Libraries.Hpricot.Hpricot.GetBufferSize)
             );
             
-            module.DefineLibraryMethod("buffer_size=", 0x21, 
+            DefineLibraryMethod(module, "buffer_size=", 0x21, 
                 new Action<IronRuby.Builtins.RubyModule, System.Int32>(IronRuby.Libraries.Hpricot.Hpricot.SetBufferSize)
             );
             
-            module.DefineLibraryMethod("scan", 0x21, 
+            DefineLibraryMethod(module, "scan", 0x21, 
                 new Func<IronRuby.Runtime.ConversionStorage<IronRuby.Builtins.MutableString>, IronRuby.Runtime.RespondToStorage, IronRuby.Runtime.BinaryOpStorage, IronRuby.Runtime.BlockParam, IronRuby.Builtins.RubyModule, System.Object, System.Object>(IronRuby.Libraries.Hpricot.Hpricot.Scan)
             );
             
         }
         
         private static void LoadIronRuby__Builtins__MutableString_Instance(IronRuby.Builtins.RubyModule/*!*/ module) {
-            module.DefineLibraryMethod("fast_xs", 0x11, 
+            DefineLibraryMethod(module, "fast_xs", 0x11, 
                 new Func<IronRuby.Runtime.RubyContext, IronRuby.Builtins.MutableString, IronRuby.Builtins.MutableString>(IronRuby.Libraries.Hpricot.MutableStringOps.FastXs)
             );
             
